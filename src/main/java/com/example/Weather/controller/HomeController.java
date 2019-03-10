065a07b311eaaa0b30eaccf4c14001356bfc759f
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-03-10 18:11:01 +0100

    refactor

--- src/main/java/com/example/Weather/controller/HomeController.java
+++ src/main/java/com/example/Weather/controller/HomeController.java
@@ -1,24 +1,28 @@
 package com.example.Weather.controller;
 
-import com.example.Weather.RequestHandler;
-import com.example.Weather.mappers.DataMapper;
-import com.example.Weather.model.Current;
+import com.example.Weather.model.Data;
+import com.example.Weather.service.CurrentService;
+import com.example.Weather.service.DataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @RequestMapping
-    public String initHomePage(Model model) throws IOException {
-        DataMapper dataMapper = new DataMapper();
-        Current currentWeather = dataMapper.MapObject(new RequestHandler()).getCurrent();
+    private CurrentService currentService;
+    private DataService dataService;
 
-        model.addAttribute("fromDate",currentWeather.getFromDateTime());
-        model.addAttribute("tillDate",currentWeather.getTillDateTime());
+    public HomeController(CurrentService currentService, DataService dataService) {
+        this.currentService = currentService;
+        this.dataService = dataService;
+    }
+
+    @RequestMapping
+    public String initHomePage(Model model)  {
+        Data data = dataService.getData(50.29748, 18.95457);
+        model.addAttribute("values", currentService.getValue(data));
+        model.addAttribute("tillDate",currentService.getTillDateTime(data));
         return "index";
     }
 
