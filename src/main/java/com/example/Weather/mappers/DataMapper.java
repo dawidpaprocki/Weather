41d7dcff6bccdf94ff90603f2e2e37deeda03695
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-03-10 14:44:03 +0100

    Update DataMapper.java

--- src/main/java/com/example/Weather/mappers/DataMapper.java
+++ src/main/java/com/example/Weather/mappers/DataMapper.java
@@ -3,17 +3,26 @@ package com.example.Weather.mappers;
 import com.example.Weather.RequestHandler;
 import com.example.Weather.model.Data;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 
 public class DataMapper {
      private ObjectMapper mapper = new ObjectMapper();
+     private static final String url = "https://airapi.airly.eu/v2/measurements/";
+     private static final String apiKey = "UM641Ep2qlOXgMAcuVjdBfTB2jmIp4rY";
 
-    public Data MapObject(RequestHandler requestHandler) throws IOException {
-       Data data;
-       return data = mapper.readValue(requestHandler.getInputStream(
-                "https://airapi.airly.eu/v2/measurements/nearest?lat=50.062006&lng=19.940984&maxDistanceKM=5",
-                "UM641Ep2qlOXgMAcuVjdBfTB2jmIp4rY"), Data.class);
+    public Data MapObject(RequestHandler requestHandler,Double latitude,Double longitude  ) throws IOException {
+        String airlyApiUrl = UrlCreator(latitude, longitude);
+        Data data;
+       return  mapper.readValue(requestHandler.getInputStream(airlyApiUrl,apiKey), Data.class);
 
+    }
+
+    private String UrlCreator(Double latitude,Double longitude){
+        return url +
+                "nearest?lat=" +
+                latitude +
+                "&lng=" +
+                longitude +
+                "&maxDistanceKM=5";
     }
 }
