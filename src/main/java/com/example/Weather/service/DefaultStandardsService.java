e86b3ffd75713d94f8f2de0c63e625ef125deb3c
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-03-27 23:29:48 +0100

    updated DefaultStandardsService - catch null values.

--- src/main/java/com/example/Weather/service/DefaultStandardsService.java
+++ src/main/java/com/example/Weather/service/DefaultStandardsService.java
@@ -17,27 +17,35 @@ public class DefaultStandardsService implements StandardsService {
         this.currentService = currentService;
     }
 
+    private Optional<Standards> getStandard(Data data, String pollutantName) {
+        Optional<Standards[]> whoStandards = Optional.ofNullable(currentService.getWHOStandards(data));
+        if (whoStandards.isPresent()) {
+            return Arrays.stream(whoStandards.get())
+                    .filter(standards -> standards.getPollutant()
+                            .equals(pollutantName))
+                    .findFirst();
+        }
+        return Optional.ofNullable(Standards.builder().build());
+    }
+
 
     @Override
-    public String getStandardName(Data data, String pollutantName) {
-        Standards[] whoStandards = currentService.getWHOStandards(data);
-        Optional<Standards> optionalName = Arrays.stream(whoStandards).filter(standards -> standards.getPollutant()
-                .equals(pollutantName))
-                .findFirst();
-        if(optionalName.isPresent()){
-            return optionalName.get().getName();
-        }else {
+    public String getStandardPollutantName(Data data, String pollutantName) {
+        Optional<Standards> standards = getStandard(data, pollutantName);
+        if (standards.isPresent()) {
+            return standards.map(Standards::getPollutant).orElse(DataResponse.LACKOFDATA.toString());
+        } else {
             return DataResponse.LACKOFDATA.toString();
         }
     }
 
     @Override
     public Double getStandardPercent(Data data, String pollutantName) {
-        Standards[] whoStandards = currentService.getWHOStandards(data);
-
-        Optional<Standards> optionalStandard = Arrays.stream(whoStandards).filter(standards -> standards.getPollutant()
-                .equals(pollutantName))
-                .findFirst();
-        return optionalStandard.map(Standards::getPercent).orElse(0.0);
+        Optional<Standards> standards = getStandard(data, pollutantName);
+        if (standards.isPresent()) {
+            return standards.map(Standards::getPercent).orElse(0.0);
+        } else {
+            return 0.0;
+        }
     }
 }
